ba1426a5d8ab800c6cbfa2d5e8687c6636994db3
mairaGalvao <mairagalvao01@gmail.com>
2022-07-18 18:59:36 +0300

    layout: changed main page theme

--- front-end-real-bill/src/Components/About.js
+++ front-end-real-bill/src/Components/About.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { Box } from "@material-ui/core";
+import { Box, Divider } from "@material-ui/core";
 import pppPic from "../images/pppPic.jpg";
 import { Link } from "react-scroll";
 import Aos from "aos";
@@ -8,6 +8,9 @@ import "aos/dist/aos.css";
 import "../css/about.css";
 
 export function About() {
+	function navNotShow() {
+		document.getElementById("navMainBox").style.display = "none";
+	}
 	let history = useHistory();
 	useEffect(() => {
 		if (Aos) {
@@ -16,64 +19,98 @@ export function About() {
 	}, [Aos]);
 	return (
 		<>
-			{/* <hr class="dividerAbout"></hr> */}
-			{/* <h1 class="livingIndexTitle">The Living index</h1> */}
-
-			{/* <img class="img" src={pppPic} /> */}
-
 			<div class="mainTitle" data-aos="fade-out">
 				<div class="subTitleMain">
 					<div class="livingIndexText">
-						<h1 class="title1" id="staticTitle">
-							Living
-						</h1>
-						<h1 class="title1" id="non-static-text">
-							{" "}
-							index
+						<h1 class="h1LivingIndex">
+							<span class="spanLivingIndex">Living</span>
+							<div class="message">
+								<div class="word1">Index</div>
+								<div class="word2">Cost</div>
+							</div>
 						</h1>
 					</div>
 					<div class="textMain">
-						<p class="textLeft">
+						<h1 class="textLeft">
 							{" "}
-							If you want to live cheap, and you can move to any country in the
-							world,
-							<br />
-							compare prices of Big Mac according to the theory of Index Big
-							Mac.
-							<br /> But why not compare the living index?{" "}
-						</p>
+							<span>An</span>
+							<span>Application</span>
+							<span>that</span>
+							<span>provides</span>
+							<span>information</span>
+							<span>about</span>
+							<span>the</span>
+							<span>living</span>
+							<span>index.</span>
+							<span>Theory</span>
+							<span>based</span>
+							<span>on</span>
+							<span>the</span>
+							<span>Purchasing</span>
+							<span>Power</span>
+							<span>Parity</span>
+							<span>.</span>
+						</h1>
 					</div>
 					<button class="btnStart">
 						{" "}
-						<div className="learnText">Learn How</div>
-						{/* <p className="btnText">Learn how</p> */}
+						<a
+							class="learnText"
+							activeclassname="active"
+							to="livingindex"
+							spy={true}
+							smooth={true}
+							onClick={() => {
+								history.push("/livingindex");
+							}}
+						>
+							Check it out
+						</a>
 					</button>
 				</div>
 
-				<div class="mainAbout">
-					<div class="textWrap">
-						<h6 class="phoneText1">
-							Living cost in more than 20.000 cities worldwide.
-						</h6>
-						<div class="smallText1">llllllllllllllllllllllllll</div>
+				<div class="basic-card basic-card-light">
+					<div class="card-content">
+						<span class="card-title">
+							The Real Bill offers the following additions
+						</span>
+						<ul>
+							<li class="card-text">
+								Living cost in more than 20.000 cities worldwide;
+							</li>
+							<Divider />
+
+							<li class="card-text">
+								Prices based on{" "}
+								<a
+									target="_blank"
+									href="https://www.numbeo.com/cost-of-living/"
+								>
+									{" "}
+									Numbeo{" "}
+								</a>{" "}
+								living costs database. It is the largest database of cost of
+								living in the world;
+							</li>
+							<Divider />
+						</ul>
 					</div>
-					<div class="textWrap">
-						<h6 class="phoneText1">
-							Living cost in more than 20.000 cities worldwide.
-						</h6>
-						<div class="smallText1">llllllllllllllllllllllllll</div>
-					</div>
-					<div class="textWrap">
-						<h6 class="phoneText1">
-							Living cost in more than 20.000 cities worldwide.
-						</h6>
-						<div class="smallText1">llllllllllllllllllllllllll</div>
-					</div>
-					<div class="textWrap">
-						<h6 class="phoneText1">
-							Living cost in more than 20.000 cities worldwide.
-						</h6>
-						<div class="smallText1">llllllllllllllllllllllllll</div>
+
+					<div class="card-link">
+						<a
+							href="#"
+							title="Life Style Info"
+							activeclassname="active"
+							spy={true}
+							smooth={true}
+							onClick={() => {
+								navNotShow();
+
+								history.push("/lifestyleinfo");
+							}}
+						>
+							<span id="lifeStyleBtn">Lifestyle Info</span>
+						</a>
 					</div>
 				</div>
 			</div>
--- front-end-real-bill/src/Components/NavBar.js
+++ front-end-real-bill/src/Components/NavBar.js
@@ -6,115 +6,119 @@ import Typography from "@material-ui/core/Typography";
 import { useHistory } from "react-router-dom";
 
 export function NavBar() {
-  let history = useHistory();
-  function navNotShow() {
-    document.getElementById("navMainBox").style.display = "none";
-  }
+	let history = useHistory();
+	function navNotShow() {
+		document.getElementById("navMainBox").style.display = "none";
+	}
 
-  return (
-    <>
-      {/* <section>
+	return (
+		<>
+			{/* <section>
         <h3 id="titleNavBar"> The Real Bill </h3>
       </section> */}
 
-      <Box id="navBox">
-        <Box style={{ display: "flex" }}>
-          <Box className="nameNavBox" style={{ display: "flex" }}>
-            <Typography
-              style={{
-                fontSize: "20px",
-                marginLeft: "5px",
-                paddingTop: "7px",
-                fontWeight: "700px",
-                paddingRight: "0",
-              }}
-              className="w3-bar-item"
-            >
-              The Reall
-            </Typography>
-            <Typography
-              className="w3-bar-item"
-              style={{
-                fontSize: "20px",
+			<Box id="navBox">
+				<Box style={{ display: "flex" }}>
+					<Box
+						className="nameNavBox"
+						style={{ display: "flex", color: "yellow" }}
+					>
+						<Typography
+							style={{
+								fontSize: "20px",
+								marginLeft: "5px",
+								paddingTop: "7px",
+								fontWeight: "700px",
+								paddingRight: "0",
+								letterSpacing: "3px",
+							}}
+							className="w3-bar-item"
+						>
+							The
+						</Typography>
+						<Typography
+							className="w3-bar-item"
+							style={{
+								fontSize: "20px",
+								letterSpacing: "2px",
+								paddingTop: "7px",
+							}}
+						>
+							<b> Real Bill </b>
+						</Typography>
+					</Box>
+				</Box>
+			</Box>
 
-                paddingTop: "7px",
-              }}
-            >
-              <b> Bill </b>
-            </Typography>
-          </Box>
-        </Box>
-      </Box>
+			<Box id="navMainBox">
+				<input
+					class="menu-icon"
+					type="checkbox"
+					id="menu-icon"
+					name="menu-icon"
+				/>
+				<label for="menu-icon"></label>
+				<nav class="nav" style={{ position: "fixed" }}>
+					<ul class="pt-5">
+						<li>
+							<a
+								activeclassname="active"
+								spy={true}
+								smooth={true}
+								onClick={() => {
+									navNotShow();
 
-      <Box id="navMainBox">
-        <input
-          class="menu-icon"
-          type="checkbox"
-          id="menu-icon"
-          name="menu-icon"
-        />
-        <label for="menu-icon"></label>
-        <nav class="nav" style={{ position: "fixed" }}>
-          <ul class="pt-5">
-            <li>
-              <a
-                activeclassname="active"
-                spy={true}
-                smooth={true}
-                onClick={() => {
-                  navNotShow();
+									history.push("/livingindex");
+								}}
+							>
+								Living Index
+							</a>
+						</li>
+						<li>
+							<a
+								activeclassname="active"
+								spy={true}
+								smooth={true}
+								onClick={() => {
+									navNotShow();
 
-                  history.push("/livingindex");
-                }}
-              >
-                Living Index
-              </a>
-            </li>
-            <li>
-              <a
-                activeclassname="active"
-                spy={true}
-                smooth={true}
-                onClick={() => {
-                  navNotShow();
+									history.push("/");
+								}}
+							>
+								The Real Bill
+							</a>
+						</li>
+						<li>
+							<a
+								activeclassname="active"
+								spy={true}
+								smooth={true}
+								onClick={() => {
+									navNotShow();
 
-                  history.push("/");
-                }}
-              >
-                The Real Bill
-              </a>
-            </li>
-            <li>
-              <a
-                activeclassname="active"
-                spy={true}
-                smooth={true}
-                onClick={() => {
-                  navNotShow();
+									history.push("/lifestyleinfo");
+								}}
+							>
+								LIfestyle Info
+							</a>
+						</li>
 
-                  history.push("/lifestyleinfo");
-                }}
-              >
-                LIfestyle Info
-              </a>
-            </li>
-
-            <li>
-              <a
-                activeclassname="active"
-                spy={true}
-                smooth={true}
-                onClick={() => {
-                  navNotShow();
-                  history.push("/contact");
-                }}
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </Box>
-    </>
-  );
+						<li>
+							<a
+								activeclassname="active"
+								spy={true}
+								smooth={true}
+								onClick={() => {
+									navNotShow();
+									history.push("/contact");
+								}}
+							>
+								Contact
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</Box>
+		</>
+	);
 }
